742314cb965ba137b7b32bd66cdfe229bd6804bc
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-13 23:45:57 +0530

    String Pallindrome

---
+++ StringPallindrome.java
@@ -0,0 +1,32 @@
+package com.demo.set3;
+
+import java.util.Scanner;
+
+public class StringPallindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter name");
+		String str=sc.next();
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+		   rev=rev+str.charAt(i);	
+		}
+		System.out.println(rev);
+	
+		if(str.equals(rev))
+		{
+			System.out.println("pallindrome");
+		}
+		else
+		{
+			System.out.println("not pallindrome");
+		}
+		sc.close();
+
+	}
+
+
+}
