14d8c9030b4f710cff04bc7e3184b36bc21f790b
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-15 08:56:44 +0530

    Character Type casting

---
+++ CharacterTypeCasting.java
@@ -0,0 +1,16 @@
+package com.demo.set5;
+
+public class CharacterTypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		char ch='a';
+//		int num=(int)ch;
+//		System.out.println(num);
+		int num=65;
+		char ch=(char)num;
+		System.out.println(ch);
+
+	}
+
+}
