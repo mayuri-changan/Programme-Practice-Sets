28063e2318b73d02527df368301bbe5254876a94
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-17 22:17:26 +0530

    Type casting

---
+++ TypeCasting.java
@@ -0,0 +1,17 @@
+package com.demo.set5;
+
+public class TypeCasting {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		double b=a;
+		System.out.println(b);
+		double c=10987.20;
+		int f=(int) c;
+		System.out.println(f);
+
+	}
+
+}
