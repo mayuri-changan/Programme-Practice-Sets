86d5908758192a90d9813f0a57ac65af4bec8769
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-18 22:58:23 +0530

    array using scanner.java

---
+++ ArrayUsingScanner.java
@@ -0,0 +1,11 @@
+package com.demo.set6;
+public class ArrayUsingScanner {
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub	
+		int []marks= {10,20,30,40,50,60,70,80,90,100};
+		for(int i=0;i<=marks.length-1;i++){
+		System.out.print(marks[i]+" ");
+		}
+	}
+
+}
