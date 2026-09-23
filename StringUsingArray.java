8d2dc908add137f24add3204611e67234ef27b8e
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-23 23:16:14 +0530

    string using array

---
+++ StringUsingArray.java
@@ -0,0 +1,22 @@
+package com.demo.set6;
+
+//import java.util.Scanner;
+
+public class StringUsingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		
+		String []s= {"Mayuri","Swapnil","changan","Rahinj"};
+		for(int i=0;i<s.length;i++)
+		{
+			System.out.print(s[i]+" ");
+		}
+		
+		
+		//sc.close();
+
+	}
+
+}
