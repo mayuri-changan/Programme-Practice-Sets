a982e8d3eaa6bd2df0214ae080e22546c178749d
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-23 23:19:29 +0530

    find 1st last element

---
+++ Findingthefirstandlastelement.java
@@ -0,0 +1,17 @@
+package com.demo.set7;
+
+public class Findingthefirstandlastelement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr= {10,20,30,40,50,60,70,80,90,100};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\nfirst element is"+ arr[0]+"\nlast element is"+arr[9]);
+		
+
+	}
+
+}
