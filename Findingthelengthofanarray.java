623194c6f1551f5b4bec32fd28ed7c69f3d9d353
mayuri-changan <mayuri2001changan@gmail.com>
2026-09-24 23:06:01 +0530

    find length of an array

---
+++ Findingthelengthofanarray.java
@@ -0,0 +1,16 @@
+package com.demo.set7;
+
+public class Findingthelengthofanarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr= {10,20,30,40,50,60,70,80,90,100};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\n length of an array is "+arr.length);
+
+	}
+
+}
